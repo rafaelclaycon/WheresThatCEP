6154644403d90e718a4186c46b622d0c43819e16
Rafael Schmitt <rafael.claycon@gmail.com>
2022-03-16 21:46:05 -0300

    Initial Commit

---
+++ WheresThatCEP/WheresThatCEPApp.swift
@@ -0,0 +1,17 @@
+//
+//  WheresThatCEPApp.swift
+//  WheresThatCEP
+//
+//  Created by Rafael Schmitt on 16/03/22.
+//
+
+import SwiftUI
+
+@main
+struct WheresThatCEPApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
